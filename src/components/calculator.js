a0cc1b011c2fb98d9dd461052ec39627cf477afa
Ankit <ankit26kushwa@gmail.comm>
2023-06-01 15:17:41 +0530

    Add logic in calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,29 +1,43 @@
 import React, { useState } from 'react';
 import Inputrow from './inputrow';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
-  const [val, setVal] = useState(0);
-
-  const onchange = (event) => {
-    event.preventDefault();
-    setVal(event.target.value);
+  const [obj, setObj] = useState({ total: 0 });
+  const onclick = (e) => {
+    setObj({ ...obj, ...calculate(obj, e) });
   };
+
   return (
     <div className="calculator">
-      <input id="result-box" type="number" value={val} onChange={onchange} />
+      <div id="result-box">
+        {(obj.total && obj.next) || obj.next ? obj.next : obj.total || 0}
+      </div>
 
-      <Inputrow item1="AC" item2="+/-" item3="%" item4="÷" />
-      <Inputrow item1="7" item2="8" item3="9" item4="×" />
-      <Inputrow item1="4" item2="5" item3="6" item4="-" />
-      <Inputrow item1="1" item2="2" item3="3" item4="+" />
+      <Inputrow item1="AC" item2="+/-" item3="%" item4="÷" onclick={onclick} />
+      <Inputrow item1="7" item2="8" item3="9" item4="×" onclick={onclick} />
+      <Inputrow item1="4" item2="5" item3="6" item4="-" onclick={onclick} />
+      <Inputrow item1="1" item2="2" item3="3" item4="+" onclick={onclick} />
       <div className="input-row input-row4">
-        <button className="zero-button" type="button">
+        <button
+          className="zero-button"
+          type="button"
+          onClick={(e) => onclick(e.target.textContent)}
+        >
           0
         </button>
-        <button className="input-item" type="button">
+        <button
+          className="input-item"
+          type="button"
+          onClick={(e) => onclick(e.target.textContent)}
+        >
           .
         </button>
-        <button className="input-item item4" type="button">
+        <button
+          className="input-item item4"
+          type="button"
+          onClick={(e) => onclick(e.target.textContent)}
+        >
           =
         </button>
       </div>
--- src/components/inputrow.js
+++ src/components/inputrow.js
@@ -2,25 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Inputitem from './inputItem';
 
-const Inputrow = (props) => {
-  const {
-    item1, item2, item3, item4,
-  } = props;
-  return (
-    <div className="input-row">
-      <Inputitem item={item1} />
-      <Inputitem item={item2} />
-      <Inputitem item={item3} />
-      <Inputitem item={item4} classN="input-item item4" />
-    </div>
-  );
-};
+const Inputrow = ({
+  item1, item2, item3, item4, onclick,
+}) => (
+  <div className="input-row">
+    <Inputitem item={item1} onclick={onclick} />
+    <Inputitem item={item2} onclick={onclick} />
+    <Inputitem item={item3} onclick={onclick} />
+    <Inputitem item={item4} classN="input-item item4" onclick={onclick} />
+  </div>
+);
 
 Inputrow.propTypes = {
   item1: PropTypes.string.isRequired,
   item2: PropTypes.string.isRequired,
   item3: PropTypes.string.isRequired,
   item4: PropTypes.string.isRequired,
+  onclick: PropTypes.func,
+};
+
+Inputrow.defaultProps = {
+  onclick: () => { },
 };
 
 export default Inputrow;
