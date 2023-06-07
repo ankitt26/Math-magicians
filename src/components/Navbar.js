8f79cefeeaaabffac26eddf5c3ac20dfef798805
Ankit <ankit26kushwa@gmail.comm>
2023-06-07 11:01:20 +0530

    Add module.css in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,21 @@
 import { Link } from 'react-router-dom';
+import styles from '../Styles/Navbar.module.css';
 
 const Navbar = () => (
   <>
-    <div>
-      <h1>Math Magicians</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/calculator">Calculator</Link>
-          </li>
-          <li>
-            <Link to="/quotes">Quotes</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className={styles.navbar}>
+      <h1 className={styles.heading}>Math Magicians</h1>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <Link to="/" className={styles.link}>Home</Link>
+        </li>
+        <li className={styles.li}>
+          <Link to="/calculator" className={styles.link}>Calculator</Link>
+        </li>
+        <li className={styles.li}>
+          <Link to="/quotes" className={styles.link}>Quotes</Link>
+        </li>
+      </ul>
     </div>
   </>
 );
